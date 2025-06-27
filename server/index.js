78dde2be602488b2ad5dd5c12abb0cacdf12914c
Tamil Selvan <tamil@sece.ac.in>
2025-06-27 12:26:36 +0530

    la\t

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(helmet());
 app.use(mongoSanitize());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
